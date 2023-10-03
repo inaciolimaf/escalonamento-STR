9c45aec7172c5300062f88ab82f257204b554540
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-10-03 09:13:41 -0300

    Adicionando código lendo do arquivo .xlsx e com classe

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-def cargaCumulativa(i, P, C, ts):
+import pandas as pd
+import re
+
+class Task:
+    def __init__(self, P, C):
+        self.P = P
+        self.C = C
+    def __str__(self):
+        return f"Período:{self.P}, custo de computação: {self.C}"
+      
+def transformarNumero(numero: str):
+    numero = re.findall(r'\d+', numero)[0]
+    return int(numero)
+def cargaCumulativa(i, tasks: [], ts:[]):
     uList = []
     wList = []
-    for t in range(1, ts):
+    for t in ts:
         u = 0
         w = 0
         for j in range(0, i):
-            w += (t/P[j])*C[j]
+            w += (t/tasks[j].P)*tasks[j].C
         u = w/t
-        uList.append(u)
-        wList.append(w)
-        
-    return {"w": wList, "u": uList}
+        if u>1:
+            print("A tarefa: {tasks[j]}, no tempo {t} não é escalonável")
         
 if __name__ == "__main__":
-    # print(cargaCumulativa(1,[60,1],[6, 0.015], 60))
-    for i in cargaCumulativa(2,[60,1],[6, 0.015], 60)['u']:
-        print(i)
-        if i>1:
-            print("ERRO")
-    
+    tasks = []
+    df = pd.read_excel("MCMV - Definições.xlsx")
+    df = df.loc[df["Core"]==1]
+    df = df.loc[df["Periódica"]=="Sim"]
+    for i, line in df.iterrows():
+        tempoComputacao = transformarNumero(line["Tempo de Computação"])
+        periodo = transformarNumero(line["Período/Deadline"])
+        tasks.append(Task(periodo, tempoComputacao))
+    for i, task in enumerate(tasks):
+        cargaCumulativa(i, tasks, range(1, 201))
+
         
